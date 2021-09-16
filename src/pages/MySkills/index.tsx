e05560154202e9dda41815d1589dce55e135d4e5
Filipe de Deus <filipedeus2015@gmail.com>
2021-09-16 13:04:10 -0300

    ajustes nas skills

--- src/pages/MySkills/index.tsx
+++ src/pages/MySkills/index.tsx
@@ -1,22 +1,9 @@
 import Header from "../../components/Header";
 import "./styles.css";
-import Footer from "../../components/Footer";
 import { Icon } from "@iconify/react";
 
 const MySkills = () => {
-  let obj = [
-    {
-      icon: "fontisto:html5",
-      name: "HTML",
-    },
-    {
-      icon: "fa-brands:css3",
-      name: "CSS",
-    },
-    {
-      icon: "akar-icons:react-fill",
-      name: "React",
-    },
+  const lingugagens = [
     {
       icon: "cib:java",
       name: "Java",
@@ -26,29 +13,15 @@ const MySkills = () => {
       name: "JavaScript",
     },
     {
-      icon: "cib:git",
-      name: "Git",
-    },
-    {
-      icon: "bi:bootstrap-fill",
-      name: "Bootstrap",
-    },
-    {
-      icon: "file-icons:docker",
-      name: "Docker",
-    },
-    {
-      icon: "fontisto:nodejs",
-      name: "NodeJs",
-    },
-    {
       icon: "akar-icons:python-fill",
       name: "Python",
     },
     {
-      icon: "akar-icons:sass-fill",
-      name: "SASS",
+      icon: "file-icons:delphi",
+      name: "Delphi",
     },
+  ]
+  const db = [
     {
       icon: "fontisto:oracle",
       name: "DB Oracle",
@@ -61,26 +34,18 @@ const MySkills = () => {
       icon: "cib:mysql",
       name: "MySQL",
     },
+  ]
+  const versionamento = [
     {
-      icon: "simple-icons:selenium",
-      name: "Selenium",
-    },
-    {
-      icon: "file-icons:cucumber",
-      name: "Cucumber",
-    },
-    {
-      icon: "cib:angular",
-      name: "Angular",
-    },
-    {
-      icon: "file-icons:delphi",
-      name: "Delphi",
+      icon: "cib:git",
+      name: "Git",
     },
     {
       icon: "file-icons:tortoisesvn",
       name: "SVN",
     },
+  ]
+  const teste = [
     {
       icon: "file-icons:karma",
       name: "Karma",
@@ -94,16 +59,99 @@ const MySkills = () => {
       name: "JUnit 5",
     },
     {
+      icon: "simple-icons:selenium",
+      name: "Selenium",
+    },
+    {
+      icon: "file-icons:cucumber",
+      name: "Cucumber",
+    },
+    {
+      icon: "simple-icons:sonarqube",
+      name: "SonarQube",
+    },
+  ]
+  const frontend = [
+    {
+      icon: "fontisto:html5",
+      name: "HTML",
+    },
+    {
+      icon: "fa-brands:css3",
+      name: "CSS",
+    },
+    {
+      icon: "akar-icons:react-fill",
+      name: "React",
+    },
+    {
+      icon: "akar-icons:sass-fill",
+      name: "SASS",
+    },
+    {
+      icon: "cib:angular",
+      name: "Angular",
+    },
+    {
+      icon: "bi:bootstrap-fill",
+      name: "Bootstrap",
+    },
+  ]
+  const backend = [
+    {
+      icon: "bx:bxl-spring-boot",
+      name: "Sprintboot",
+    },
+    {
+      icon: "fontisto:nodejs",
+      name: "NodeJs",
+    },
+  ]
+  const cicd = [
+    {
+      icon: "file-icons:docker",
+      name: "Docker",
+    },
+    {
+      icon: "bx:bxl-kubernetes",
+      name: "Kubernetes",
+    },
+    {
+      icon: "cib:jenkins",
+      name: "Jenkins",
+    },
+    {
+      icon: "cib:quarkus",
+      name: "Quarkus",
+    },
+  ]
+  const pacotes = [
+    {
+      icon: 'akar-icons:npm-fill',
+      name: 'NPM'
+    },
+    {
       icon: "simple-icons:apachemaven",
       name: "Maven",
     },
-  ];
+  ]
+
+  function handleTopPage() {
+    if (window.scrollX === 0 && window.screenY === 0) {
+      return;
+    }
+    window.scrollTo(0, 0);
+  }
+
   return (
     <>
       <Header />
-      <p className="white-title title">Minhas skills</p>
-      <div className="myskills-container">
-        {obj.map((item, idx) => (
+      <div className="top-sumary-container" onClick={handleTopPage}>
+        <Icon className="top-icon" icon="akar-icons:circle-chevron-up-fill" fontSize="70" color="#00DF5E" />
+      </div>
+      <p className="white-title title" id="linguagens">Linguagens</p>
+      <div className="myskills-container"> 
+        {lingugagens.map((item, idx) => (
           <div className="skill" key={idx}>
             <div>
               <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
@@ -112,7 +160,83 @@ const MySkills = () => {
           </div>
         ))}
       </div>
-      <Footer />
+      <p className="white-title title" id="frontend">Frontend</p>
+      <div className="myskills-container">
+        {frontend.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="backend">Backend</p>
+      <div className="myskills-container">
+        {backend.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="cicd">CI/CD | Containers</p>
+      <div className="myskills-container">
+        {cicd.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="pacotes">Gerenciador de pacotes</p>
+      <div className="myskills-container">
+        {pacotes.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="db">Database</p>
+      <div className="myskills-container">
+        {db.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="versionamento">Versionamento</p>
+      <div className="myskills-container">
+        {versionamento.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
+      <p className="white-title title" id="testes">Testes</p>
+      <div className="myskills-container">
+        {teste.map((item, idx) => (
+          <div className="skill" key={idx}>
+            <div>
+              <Icon icon={item.icon} color="#00DF5E" fontSize="90" />
+            </div>
+            <p className="white-title description">{item.name}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
