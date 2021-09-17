56ea6a3f5a1736542e6d12693b2b7c893a07ffa0
Filipe de Deus <filipedeus2015@gmail.com>
2021-09-16 21:49:10 -0300

    links no info

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -10,7 +10,7 @@ const Info = () => {
             <Icon icon="akar-icons:github-fill" fontSize="30" color="#00DF5E" />
           </div>
           <div className="item-content">
-            <p className="item-title">Github</p>
+            <a href="https://github.com/dedeus6" target="_blank" rel="noreferrer"><p className="item-title">Github</p></a>
             <p className="item-subtitle">/dedeus6</p>
           </div>
         </div>
@@ -21,7 +21,7 @@ const Info = () => {
             <Icon icon="entypo:email" fontSize="30" color="#00DF5E" />
           </div>
           <div className="item-content">
-            <p className="item-title">Email</p>
+          <a href="mailto:filipededeus2015@gmail.com" target="_blank" rel="noreferrer"><p className="item-title">Email</p></a>
             <p className="item-subtitle">filipedeus2015@gmail.com</p>
           </div>
         </div>
@@ -32,7 +32,7 @@ const Info = () => {
             <Icon icon="akar-icons:whatsapp-fill" fontSize="30" color="#00DF5E" />
           </div>
           <div className="item-content">
-            <p className="item-title">WhatsApp</p>
+            <a href="https://api.whatsapp.com/send?I=pt_BR&phone=5561983559013" target="_blank" rel="noreferrer"><p className="item-title">WhatsApp</p></a>
             <p className="item-subtitle">+55 (61) 98355-9013</p>
           </div>
         </div>
@@ -43,7 +43,7 @@ const Info = () => {
             <Icon icon="akar-icons:linkedin-fill" fontSize="30" color="#00DF5E" />
           </div>
           <div className="item-content">
-            <p className="item-title">LinkedIn</p>
+          <a href="https://www.linkedin.com/in/dedeus6/" target="_blank" rel="noreferrer"><p className="item-title">LinkedIn</p></a>
             <p className="item-subtitle">/dedeus6</p>
           </div>
         </div>
